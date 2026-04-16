1d6ccb796d763e6e28ef7f6a7c369f92d5dd27bd
Voyrox <ewen@macculloch.net>
2026-04-16 16:58:26 +1000

    Fix gitignore to track src/history

---
+++ src/history/types.go
@@ -0,0 +1,55 @@
+package history
+
+import (
+	"sync"
+	"time"
+)
+
+type ServiceSnapshot struct {
+	ServiceName         string  `json:"serviceName"`
+	SystemID            int     `json:"systemId"`
+	CurrentReplicas     int     `json:"currentReplicas"`
+	RecommendedReplicas int     `json:"recommendedReplicas"`
+	AppliedReplicas     int     `json:"appliedReplicas"`
+	AvgCPU              float64 `json:"avgCPU"`
+	AvgMem              float64 `json:"avgMem"`
+	RPS                 float64 `json:"rps"`
+	MetricsReady        bool    `json:"metricsReady"`
+}
+
+type ClusterSnapshot struct {
+	Timestamp           time.Time         `json:"timestamp"`
+	TotalReplicas       int               `json:"totalReplicas"`
+	RecommendedReplicas int               `json:"recommendedReplicas"`
+	AppliedReplicas     int               `json:"appliedReplicas"`
+	AvgCPU              float64           `json:"avgCPU"`
+	AvgMem              float64           `json:"avgMem"`
+	TotalRPS            float64           `json:"totalRps"`
+	MetricsReady        bool              `json:"metricsReady"`
+	Services            []ServiceSnapshot `json:"services"`
+}
+
+type DayHistory struct {
+	Date      string            `json:"date"`
+	Snapshots []ClusterSnapshot `json:"snapshots"`
+}
+
+type Response struct {
+	Days   []DayHistory     `json:"days"`
+	Latest *ClusterSnapshot `json:"latest,omitempty"`
+}
+
+type StoreImpl struct {
+	dir string
+	mu  sync.Mutex
+}
+
+type dayFile struct {
+	Date      string
+	Snapshots map[int]ClusterSnapshot
+}
+
+type datedFile struct {
+	date time.Time
+	path string
+}
